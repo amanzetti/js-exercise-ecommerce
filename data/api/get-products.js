8dba7364be774cd967d2f61fff58837dd686e203
Alessandro Manzetti <alessandro.manzetti@spindox.it>
2024-01-21 02:20:38 +0100

    add try catch

--- data/api/get-products.js
+++ data/api/get-products.js
@@ -2,9 +2,15 @@ import { HEADERS_AUTHORIZATION, URL } from "../../assets/const.js";
 import Product from "../models/product.model.js";
 
 const _getProductsAPI = async function () {
-  const resp = await fetch(URL, {
-    headers: HEADERS_AUTHORIZATION,
-  });
+  let resp;
+  
+  try {
+    resp = await fetch(URL, {
+      headers: HEADERS_AUTHORIZATION,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 
   return resp.json();
 };
